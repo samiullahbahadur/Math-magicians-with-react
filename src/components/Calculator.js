094c8b4ba96c3e8908c562f9994c611d9c9f207d
samiullahbahadur <bahadursamiullah@gmail.com>
2022-03-23 15:19:27 +0430

    Fix linters errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,169 +1,79 @@
-import { useState, useEffect } from 'react';
-import NumberFormat from 'react-number-format';
+import react from 'react';
+import '../App.css';
 
-function Calculator() {
-  const [preState, setPreState] = useState('');
-  const [curState, setCurState] = useState('');
-  const [input, setInput] = useState('0');
-  const [operator, setOperator] = useState(null);
-  const [total, setTotal] = useState(false);
+class Calculator extends react.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-  const inputNum = (e) => {
-    if (curState.includes('.') && e.target.innerText === '.') return;
+  render() {
+    return (
+      <div className="container">
+        <div className="wrapper">
+          <div className="screen">0</div>
 
-    if (total) {
-      setPreState('');
-    }
-
-    curState
-      ? setCurState((pre) => pre + e.target.innerText)
-      : setCurState(e.target.innerText);
-    setTotal(false);
-  };
-
-  useEffect(() => {
-    setInput(curState);
-  }, [curState]);
-
-  useEffect(() => {
-    setInput('0');
-  }, []);
-  const operatorType = (e) => {
-    setTotal(false);
-    setOperator(e.target.innerText);
-    if (curState === '') return;
-    if (preState !== '') {
-      equals();
-    } else {
-      setPreState(curState);
-      setCurState('');
-    }
-  };
-
-  const equals = (e) => {
-    if (e?.target.innerText === '=') {
-      setTotal(true);
-    }
-    let cal;
-    switch (operator) {
-      case '÷':
-        cal = String(parseFloat(preState) / parseFloat(curState));
-        break;
-
-      case '+':
-        cal = String(parseFloat(preState) + parseFloat(curState));
-        break;
-      case 'X':
-        cal = String(parseFloat(preState) * parseFloat(curState));
-        break;
-      case '-':
-        cal = String(parseFloat(preState) - parseFloat(curState));
-        break;
-      default:
-        return;
-    }
-    setInput('');
-    setPreState(cal);
-    setCurState('');
-  };
-
-  const minusPlus = () => {
-    if (curState.charAt(0) === '-') {
-      setCurState(curState.substring(1));
-    } else {
-      setCurState(`-${curState}`);
-    }
-  };
-
-  const percent = () => {
-    preState
-      ? setCurState(String((parseFloat(curState) / 100) * preState))
-      : setCurState(String(parseFloat(curState) / 100));
-  };
-
-  const reset = () => {
-    setPreState('');
-    setCurState('');
-    setInput('0');
-  };
-  return (
-    <div className="container">
-      <div className="wrapper">
-        <div className="screen">
-          {input !== '' || input === '0' ? (
-            <NumberFormat
-              value={input}
-              displayType="text"
-              thousandSeparator
-            />
-          ) : (
-            <NumberFormat
-              value={preState}
-              displayType="text"
-              thousandSeparator
-            />
-          )}
-        </div>
-        <div className="btn light-gray" onClick={reset}>
-          AC
-        </div>
-        <div className="btn light-gray" onClick={minusPlus}>
-          +/-
-        </div>
-        <div className="btn light-gray row" onClick={percent}>
-          %
-        </div>
-        <div className="btn orange" onClick={operatorType}>
-          ÷
-        </div>
-        <div className="btn" onClick={inputNum}>
-          7
-        </div>
-        <div className="btn" onClick={inputNum}>
-          8
-        </div>
-        <div className="btn row" onClick={inputNum}>
-          9
-        </div>
-        <div className="btn orange" onClick={operatorType}>
-          X
-        </div>
-        <div className="btn" onClick={inputNum}>
-          4
-        </div>
-        <div className="btn" onClick={inputNum}>
-          5
-        </div>
-        <div className="btn row" onClick={inputNum}>
-          6
-        </div>
-        <div className="btn orange" onClick={operatorType}>
-          -
-        </div>
-        <div className="btn" onClick={inputNum}>
-          1
-        </div>
-        <div className="btn" onClick={inputNum}>
-          2
-        </div>
-        <div className="btn row" onClick={inputNum}>
-          3
-        </div>
-        <div className="btn orange" onClick={operatorType}>
-          +
-        </div>
-        <div className="btn zero" onClick={inputNum}>
-          0
-        </div>
-        <div className="btn row1" onClick={inputNum}>
-          .
-        </div>
-        <div className="btn orange" onClick={equals}>
-          =
+          <button type="button" className="btn light-gray">
+            AC
+          </button>
+          <button type="button" className="btn light-gray">
+            +/-
+          </button>
+          <button type="button" className="btn light-gray row">
+            %
+          </button>
+          <button type="button" className="btn orange">
+            ÷
+          </button>
+          <button type="button" className="btn">
+            7
+          </button>
+          <button type="button" className="btn">
+            8
+          </button>
+          <button type="button" className="btn row">
+            9
+          </button>
+          <button type="button" className="btn orange">
+            X
+          </button>
+          <button type="button" className="btn">
+            4
+          </button>
+          <button type="button" className="btn">
+            5
+          </button>
+          <button type="button" className="btn row">
+            6
+          </button>
+          <button type="button" className="btn orange">
+            -
+          </button>
+          <button type="button" className="btn">
+            1
+          </button>
+          <button type="button" className="btn">
+            2
+          </button>
+          <button type="button" className="btn row">
+            3
+          </button>
+          <button type="button" className="btn orange">
+            +
+          </button>
+          <button type="button" className="btn zero">
+            0
+          </button>
+          <button type="button" className="btn row1">
+            .
+          </button>
+          <button type="button" className="btn orange">
+            =
+          </button>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Calculator;
