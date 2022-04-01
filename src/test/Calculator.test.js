efb381840aa2eb11e2f7039470e268305a89fc0d
Okoroji Victor Ebube <okorojiebube2@gmail.com>
2022-04-01 11:00:10 -0500

    Update Calculator.test.js

--- src/test/Calculator.test.js
+++ src/test/Calculator.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Calculator from '../components/Calculator';
 
 it('renders correctly', () => {
   const tree = render(<Calculator />);
   expect(tree).toMatchSnapshot();
 });
+
+it('Expects 10 + 10 to equal 20', () => {
+    const tree = render(<Calculator />);
+    fireEvent.click(tree.querySelector('.ac'));
+    fireEvent.click(screen.getByText('1'));
+    fireEvent.click(screen.getByText('0'));
+    fireEvent.click(screen.getByText('+'));
+    fireEvent.click(screen.getByText('1'));
+    fireEvent.click(screen.getByText('0'));
+    fireEvent.click(screen.getByText('='));
+    expect(container.querySelector('.result').innerHTML).toBe('20');
+  });
