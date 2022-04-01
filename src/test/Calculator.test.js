3c049685083f0ec49d6cd7eb24970947132130e0
samiullahbahadur <bahadursamiullah@gmail.com>
2022-04-01 18:39:29 +0430

    Fix linters error

--- src/test/Calculator.test.js
+++ src/test/Calculator.test.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import Calculator from "../components/Calculator";
+import React from 'react';
+import { render } from '@testing-library/react';
+import Calculator from '../components/Calculator';
 
-it("renders correctly", () => {
+it('renders correctly', () => {
   const tree = render(<Calculator />);
   expect(tree).toMatchSnapshot();
 });
